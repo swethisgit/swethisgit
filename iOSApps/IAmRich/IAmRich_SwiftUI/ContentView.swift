0df68e2a86036c6e5599557a69c83c655fc7ffa8
swedathiyagarajan <sweda@ventunotech.com>
2024-01-19 16:58:22 +0530

    I am rich

---
+++ iOSApps/IAmRich/IAmRich_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I am Rich
+//
+//  Created by Sweda Thiyagarajan on 21/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSApps/IAmRich/IAmRich_SwiftUI/I_am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_RichApp.swift
+//  I am Rich
+//
+//  Created by Sweda Thiyagarajan on 21/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct I_am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
