95dbeba5021a3cd0390ac46fa056c81d3973b3aa
swedathiyagarajan <sweda@ventunotech.com>
2024-01-19 16:47:31 +0530

    CoreDataTest

---
+++ iOSApps/CoreDataTestApp/CoreDataTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataTestApp
+//
+//  Created by Sweda Thiyagarajan on 30/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
