2a423886b79c60b9764f04a4a170d4a9a1de4e5b
swedathiyagarajan <sweda@ventunotech.com>
2024-01-19 16:36:14 +0530

    BMI

---
+++ iOSApps/BMI-Calculator/BMICalculator/Models/BMI .swift
@@ -0,0 +1,15 @@
+//
+//  BMI .swift
+//  BMI Calculator
+//
+//  Created by Sweda Thiyagarajan on 07/10/2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+import UIKit
+struct BMI {
+    let BMI : Float
+    let advice : String
+    let color : UIColor
+}
