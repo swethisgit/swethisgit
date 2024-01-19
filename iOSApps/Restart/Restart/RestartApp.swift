7024a0a89cd2402893d85a3cd03523b1d06ee66c
swedathiyagarajan <swethimail@gmail.com>
2024-01-19 17:41:57 +0530

    Restart

---
+++ iOSApps/Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Sweda Thiyagarajan on 19/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
