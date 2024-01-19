3a11a633284dbc5bb53ed5320f3404040d14e301
swedathiyagarajan <sweda@ventunotech.com>
2024-01-19 16:40:47 +0530

    ByteCoin

---
+++ iOSApps/ByteCoin/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Sweda Thiyagarajan on 14/10/2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct CoinData: Codable {
+    let rate: Double
+    let asset_id_quote: String
+}
