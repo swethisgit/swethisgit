7e4fedfff90157ae5ea383bd0696edcf11a8bbe0
swedathiyagarajan <sweda@ventunotech.com>
2024-01-19 16:57:54 +0530

    I am poor

---
+++ iOSApps/IAmPoor/IAmPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Sweda Thiyagarajan on 23/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
