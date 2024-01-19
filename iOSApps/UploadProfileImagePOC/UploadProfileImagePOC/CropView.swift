c3766699f061f96c110fec099fccc39bfd43a05a
swedathiyagarajan <swethimail@gmail.com>
2024-01-19 17:48:18 +0530

    next set of projects

---
+++ iOSApps/UploadProfileImagePOC/UploadProfileImagePOC/CropView.swift
@@ -0,0 +1,22 @@
+//
+//  CropView.swift
+//  UploadProfileImagePOC
+//
+//  Created by Sweda Thiyagarajan on 31/05/2022.
+//
+
+import UIKit
+
+class CropView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    override func point(inside point: CGPoint, with event:   UIEvent?) -> Bool {
+        return false
+    }
+}
